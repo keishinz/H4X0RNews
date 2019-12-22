04c972a9bf972097f0cb4bc59070ea1625c36589
Keishin CHOU <keishin.chou@gmail.com>
2019-12-22 16:48:45 +0900

    Initial Commit

---
+++ H4X0RNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0RNews
+//
+//  Created by Keishin CHOU on 2019/12/22.
+//  Copyright © 2019 Keishin CHOU. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
